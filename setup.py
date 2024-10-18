eb3f17f4f27ece165422300654a13d2379208af5
Shreyas Kulkarni <shreyaskulkarni75@outlook.com>
2024-10-18 20:15:20 +0530

    src commit second

--- setup.py
+++ setup.py
@@ -4,9 +4,6 @@ from typing import List
 HYPEN_E_DOT = '-e .'
 
 def get_requirements(file_path : str) -> List[str]:
-    """
-    This Function Will return the List of requirements
-    """
     requirements = []
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
